13d61ed0a012032c58b4b5d2e4a28bbbdb393dcc
AsyncSan <gojiita@hotmail.de>
2023-01-23 16:27:42 +0100

    trying p5.js

---
+++ assets/js/new-search.js
@@ -0,0 +1,5 @@
+function new_search() {
+    $('.new-search-wrapper').addClass('active');
+    $('.new-search-form').addClass('active');
+    $('.new-search-field').simpleJekyllSearch();
+  }
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,13 +1,23 @@
 //<script src="assets\js\p5.js"></script>
 function setup() {
-    createCanvas(400, 400);
+      // check if the device is running on mobile
+  isMobile = (navigator.userAgent.indexOf("Mobile") !== -1);
+
+  if (isMobile) {
+    var canvas = createCanvas(windowWidth, windowHeight);
+
+    canvas.parent('container');
+  }
   }
   
   function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
+    if (isMobile) {
+      if (mouseIsPressed) {
+        fill(0);
+      } else {
+        fill(255);
+      }
+      ellipse(mouseX, mouseY, 80, 80);
     }
-    ellipse(mouseX, mouseY, 80, 80);
+
   }
