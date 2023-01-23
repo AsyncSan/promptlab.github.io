9475a9a1c510db60b515fe63dd3b259637405014
AsyncSan <gojiita@hotmail.de>
2023-01-23 13:04:17 +0100

    p5.js

---
+++ assets/js/sketch.js
@@ -0,0 +1,13 @@
+//<script src="assets\js\p5.js"></script>
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
